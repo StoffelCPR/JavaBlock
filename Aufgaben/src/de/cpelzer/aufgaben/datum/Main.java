386ed49a142d3c879d8ba37a143af12e09b3c618
Christoph Pelzer <christoph.pelzer@kisters.de>
2017-10-13 10:39:19 +0200

    getWochentag() eingefügt && JUnits ergänzt && JavaDoc ergänzt

--- Aufgaben/src/de/cpelzer/aufgaben/datum/Main.java
+++ Aufgaben/src/de/cpelzer/aufgaben/datum/Main.java
@@ -8,22 +8,16 @@ package de.cpelzer.aufgaben.datum;
 public class Main {
 
 	public static void main(String[] args) {
-		Datum d1 = new Datum(330, 2017);
-		System.out.println("Das ist D1 " + d1);
+		Datum d1 = new Datum(60, 2016);
 		
-		System.out.println("Von d1 is tmorgen "  + d1.morgen());
-		System.out.println("Von d1 war gestern " + d1.gestern());
+		System.out.println(d1.toString());
 		
-		Datum d2 = new Datum(29,2,2016);
 		
-		System.out.println("d2 morgen " + d2.morgen());
-		System.out.println("d2 gestern " + d2.gestern());
+		Datum d2 = new Datum(59, 2017);
+		System.out.println(d2.toString());
 		
-		System.out.println(d2);
-		System.out.println(d1.equals(d2));
-		System.out.println(d1.isGleicherTag(d2));
-		
-
+		Datum d3 = new Datum(60, 2017);
+		System.out.println(d3.toString());
 	}
 
 }
